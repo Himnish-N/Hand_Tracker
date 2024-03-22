08645a7f15c5cfcef7c01ca88ef128c74e5f57a9
Himnish-N <pes1202101226@pesu.pes.edu>
2024-03-22 23:36:22 +0530

    minimal hand tracker final

--- Hand_Tracking_Min.py
+++ Hand_Tracking_Min.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands() #initialize mediapipe hand object
@@ -11,6 +11,9 @@ max_num_hands,
 min_detection_confidence,
 min_tracking_confidence'''
 mpDraw = mp.solutions.drawing_utils
+
+prevtime = 0
+curtime = 0
 
 while True: # run the camera
     success, img = cap.read()
@@ -22,8 +25,20 @@ while True: # run the camera
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy) #gives us the id and the position of the landmark in integer
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) # draws landmarks on hands
 
+
+    curtime = time.time()
+    fps = 1/(curtime-prevtime)
+    prevtime = curtime
+
+    cv2.putText(img, str(int(fps)),(10,70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,0,255),3) # gives us the fps of the image
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
